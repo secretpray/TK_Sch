2552e0cea37b3cc418735dff565750de10a5ae6b
secretpray <a.p.ruban@gmail.com>
2020-08-05 19:01:56 +0300

    Lesson 2

--- Lesson 2/6_sale.rb
+++ Lesson 2/6_sale.rb
@@ -10,36 +10,39 @@
 
 shop_cart = {}
 total_price = 0
-delimiter = "-*-" * 20 # для красоты
+delimiter = "-*-" * 15 # для красоты
 
 # Получаем данные от пользователя
 
 puts delimiter
 
 loop do
-  puts "Введите название товара или stop для выхода."
+  puts "Введите название товара или stop (стоп) для выхода."
   item_name = gets.force_encoding(Encoding::UTF_8).chomp
-  break if item_name == 'stop'
+  break if item_name == 'stop' || item_name == 'стоп'
   puts "Введите цену товара (за единицу или 100 грамм):"
   item_price = gets.to_f
   puts "Введите количество товара (шт или грамм):"
   item_quantity = gets.to_f
   shop_cart[item_name] = { item_price: item_price, item_quantity: item_quantity, item_amount: item_price * item_quantity }
-  puts "Добавлен продукт: #{item_name.downcase} ценой #{item_price} в количестве #{item_quantity.to_i} "
+  puts "Добавлен продукт: #{item_name.downcase} ценой #{item_price} грн в количестве #{item_quantity.to_i}"
+  total_price += item_price * item_quantity
 end
 
+# Если вне цикла надо подсчитать общую стоимость покупок:
 # total_price = shop_cart.sum {|item_price:, item_quantity:, item_amount:| item_amount } # deprecated in Ruby 2.7
+# shop_cart.each { |item, data| total_price += data[:item_amount] } # рабочее решение (.each_pair) !
 
-# покажем весь хеш
+# Покажем хеш
 p shop_cart
 
 p delimiter
 
 # Покажем корзину с расшифровкой
 shop_cart.each do |item_name, data|
-  puts "- #{item_name}: #{data[:item_price].to_s} x #{data[:item_quantity]} = #{data[:item_amount]} грн."
-  total_price += data[:item_amount]
-end
+   puts "- #{item_name.downcase}: #{data[:item_price].to_s} x #{data[:item_quantity]} = #{data[:item_amount]} грн."
+#   total_price += data[:item_amount] # в данному случае лишняя операция
+ end
 
 puts delimiter
 
@@ -47,6 +50,8 @@ puts delimiter
 puts "Общая сумма покупок: #{total_price}"
 
 # To Do
-# 1) Добавить красивое псевдо-меню для выбора опций
-# 2) Проверить на корретность вводимых данных
-# 3) Предусмотреть возможность обновления (добавления) товара с идентичным именем.
+# 1) Добавить красивое псевдо-меню для выбора опций (Добавить, Обновить, Удалить, Выход)
+# 2) Проверить на корретность вводимых данных (Строки вместо цифр в цене и количестве товара)
+# 3) Предусмотреть возможность обновления и удаления товара с идентичным именем.
+# 4) С дробными цифрами (грамм) надо рахобраться
+# 5) Как пересчитать граммы - килограммы
